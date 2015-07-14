2c0efae24572f7fff86804953ca9437cc38567fd
Luke Channings <luke@channings.me>
2015-07-14 20:13:01 +0100

    Initial Commit

---
+++ EasyList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyList
+//
+//  Created by Luke Channings on 14/07/2015.
+//  Copyright © 2015 Luke Channings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
